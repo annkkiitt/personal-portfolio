e1ba952e4f4630eab2fbd95ce05aa9afd6990cc4
ankit.r@cloud202.com <ankit.r@cloud202.com>
2025-08-21 22:55:44 +0530

    feat: Project 3d Icon added

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import AboutMe from "@/public/images/About.png"
 import Plasma from "@/components/Plasma/Plasma"
 import MailIcon from "@/public/icons/3dicons-mail-iso-color.png"
 import SkillIcon from "@/public/icons/3dicons-lab-dynamic-color.png"
+import ProjectIcon from "@/public/icons/3dicons-computer-dynamic-color.png"
 
 export default function HomePage() {
   return (
@@ -36,8 +37,19 @@ export default function HomePage() {
           </div>
 
           {/* Intro - Large */}
-          <div className="col-span-1 sm:col-span-2 row-span-1 sm:row-span-2 md:row-span-2 lg:row-span-3">
-              <div className="h-full p-4 sm:p-6 flex flex-col justify-center">
+          <div className="col-span-1 relative sm:col-span-2 row-span-1 sm:row-span-2 md:row-span-2 lg:row-span-3">
+
+            <div className="h-full relative z-10 p-4 sm:p-6 flex flex-col justify-center border rounded-2xl sm:rounded-4xl">
+              <div className="absolute z-10 inset-0 w-full h-full rounded-2xl sm:rounded-4xl">
+                <Plasma
+                  color="#8B5DFF"
+                  speed={0.3}
+                  direction="reverse"
+                  scale={2}
+                  opacity={0.5}
+                  mouseInteractive={true}
+                />
+              </div>
                 <p className="text-xs sm:text-sm md:text-lg text-center sm:text-end text-muted-foreground">
                  Hi I&apos;m
                 </p>
@@ -49,36 +61,25 @@ export default function HomePage() {
           </div>
 
           {/* Projects - Large */}
-          <div className="col-span-1 row-span-1 md:row-span-2 lg:row-span-3 h-[300px] w-full relative overflow-hidden rounded-2xl sm:rounded-4xl">
-            {/* Plasma Background */}
-            <div className="absolute inset-0 z-10 w-full h-full">
-              <Plasma
-                color="#8B5DFF"
-                speed={0.3}
-                direction="reverse"
-                scale={1.1}
-                opacity={1}
-                mouseInteractive={true}
-              />
-            </div>
+          <div className="col-span-1 row-span-1 md:row-span-2 lg:row-span-3 h-[300px] w-full overflow-hidden rounded-2xl sm:rounded-4xl">
             
             {/* Content */}
-            <Link href="/projects" className="block h-full relative z-10">
-              <div className="h-full bg-transparent border-2 border-border rounded-2xl sm:rounded-4xl p-4 sm:p-6 flex flex-col">
+            <Link href="/projects" className="block h-full relative z-10 bg-[#F05454]/80">
+              <div className="h-full bg-transparent border-2 border-border group rounded-2xl sm:rounded-4xl p-4 sm:p-6 flex flex-col">
                 <div className="flex items-center justify-between mb-3 sm:mb-4">
-                  <div className="p-2 rounded-lg bg-primary/10">
-                    <Briefcase className="h-4 w-4 sm:h-5 sm:w-5" />
+                  <div className="p-1 rounded-lg bg-primary/10 group-hover:scale-110 group-hover:-rotate-6 transition-all duration-300">
+                    <Image src={ProjectIcon.src} alt="Projects" width={50} height={50} />
                   </div>
                 </div>
-                <h2 className="text-base sm:text-lg font-semibold mb-2 text-white">Projects</h2>
-                <p className="text-xs sm:text-sm text-white flex-grow">
+                <h2 className="text-base sm:text-lg md:text-2xl font-semibold mb-2 text-black/80 uppercase">Projects</h2>
+                <p className="text-xs sm:text-sm text-black/80 flex-grow">
                   Explore my portfolio of web applications, tools, and creative solutions.
                 </p>
               </div>
             </Link>
           </div>
           {/* Skills - Medium */}
-          <div className="col-span-2">
+          <div className="col-span-1">
             <Link href="/skills" className="block h-full ">
               <div className="h-full bg-[#379777] border-2 border-border rounded-xl sm:rounded-3xl p-4 sm:p-6 flex flex-col group ">
                 <div className="flex items-center justify-between mb-3 sm:mb-4">
@@ -95,9 +96,9 @@ export default function HomePage() {
           </div>
 
                     {/* Contact - Small */}
-            <div className="col-span-1">
+            <div className="col-span-2">
             <Link href="/contact" className="block h-full">
-              <div className="h-full bg-card border-2 border-border rounded-xl sm:rounded-3xl p-4 sm:p-6 flex flex-col group hover:scale-105 transition-all duration-300">
+              <div className="h-full bg-card border-2 border-border rounded-xl sm:rounded-3xl p-4 sm:p-6 flex flex-col group ">
                 <div className="flex items-center justify-between mb-3 sm:mb-4">
                   <div className="p-1 rounded-lg bg-primary/5 group-hover:scale-105 group-hover:rotate-6 transition-all duration-300">
                     <Image src={MailIcon.src} alt="Mail" width={60} height={60} />
